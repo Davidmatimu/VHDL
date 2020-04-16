c1b3b9467e7b42c083224bb6ce88480a9a84dfd0
David <dmatimu@udel.edu>
2020-04-16 00:07:52 -0400

    Create 4bit_shift_reg.vhd

---
+++ 4bit_shift_reg.vhd
@@ -0,0 +1,19 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity bitreg_shift is
+    Port ( I : in STD_LOGIC_VECTOR (3 downto 0);
+           I_Shift_In : in STD_LOGIC;
+           sel : in STD_LOGIC_VECTOR (3 downto 0);-- 00:hold; 01: shift left; 10: shift right; 11: load
+           clock : in STD_LOGIC;
+           enable : in STD_LOGIC;
+           O : out STD_LOGIC_VECTOR (3 downto 0));
+end bitreg_shift;
+
+architecture Behavioral of bitreg_shift is
+
+begin
+
+
+end Behavioral;
