14c0bc76fa10a073372288e3e29a3f757f47fb48
bAbybLAckbeAr <sam.wendt333@gmail.com>
2020-04-16 21:44:43 -0400

    wrote code for 4bit shift register. Have not tested code yet. added benchmark file for 4-1 mux

--- 4bit_shift_reg.vhd
+++ 4bit_shift_reg.vhd
@@ -12,9 +12,28 @@ entity bitreg_shift is
 end bitreg_shift;
 
 architecture Behavioral of bitreg_shift is
+signal sig : STD_LOGIC_VECTOR (3 downto 0) := "0000"; --start signal at 0
 begin
 shift:process(I, I_SHIFT_IN, sel, clock, enable)
 begin
-
+if enable = '0' then
+    O <= "0000";
+elsif (clock-event and clock = '1' and enable = '1') then
+    if sel = "00" then --hold, ie do nothing
+    elsif sel = "01" then --shift left
+        sig(3) <= sig(2);
+        sig(2) <= sig(1);
+        sig(1) <= sig(0);
+        sig(0) <= I_Shift_In;
+    elsif sel = "10" then --shift right
+        sig(3) <= I_Shift_In;
+        sig(2) <= sig(3);
+        sig(1) <= sig(2);
+        sig(0) <= sig(1);
+    elsif sel = "11" then --load
+        sig <= I;
+    endif;
+endif;
 end process shift;
+O <= sig;
 end Behavioral;
